f99b4932267ae25e0893c1bcab3264909c64ca7e
ReymouldRevanth <revanth.danduboina@reymould.com>
2023-09-29 18:38:47 +0530

    branding page content updated

--- form-function/contact-form.php
+++ form-function/contact-form.php
@@ -8,8 +8,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $subject = "Enqiry from Reymould Website";
     $message = $_POST["message"];
 
+    $email_body = "Name : ".$name."Email : ".$email."Mobile Number : ".$phone."Message : ".$message;
 
-    if (mail($to, $subject, $message)) {
+    $header = "From <noreply@reymould.com>";
+
+    if (mail($to, $subject, $email_body,$header)) {
         header("Location: ../index.html?emailStatus=enqiryPass");
         exit;
     } else {
