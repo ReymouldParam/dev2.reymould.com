28343d1d9e3ee0319f806ebe5bcb3b9fe88a5aba
ReymouldRevanth <revanth.danduboina@reymould.com>
2023-10-15 01:02:51 +0530

    Service page content aligned

--- form-function/contact-form.php
+++ form-function/contact-form.php
@@ -8,11 +8,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $subject = "Enqiry from Reymould Website";
     $message = $_POST["message"];
 
-    $email_body = "Name : ".$name."Email : ".$email."Mobile Number : ".$phone."Message : ".$message;
+    $email_body = "Name : ".$name."\nEmail : ".$email."\nMobile Number : ".$phone."\nMessage : ".$message;
 
-    // $header = "From: <noreply@reymould.com>";
+    $header = "From: <".$email.">";
 
-    if (mail($to, $subject, $email_body)) {
+    if (mail($to, $subject, $email_body,$header)) {
         header("Location: ../contactus?emailStatus=enqiryPass");
         exit;
     } else {
