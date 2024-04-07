669deae37a2d9ec62a7c4ba25acc182451f5941a
Revant Danduboina <revanth.danduboina@reymould.com>
2024-04-07 08:33:42 +0530

    cursor animations updated

--- js/script.js
+++ js/script.js
@@ -865,38 +865,38 @@ var THEMEMASCOT = {};
 
 
 	// Mouse Circle Animation 
-	var $circle = $('.circle'),
-		$follow = $('.circle-follow');
-	function moveCircle(e) {
-		TweenLite.to($circle, 0.3, {
-			x: e.clientX,
-			y: e.clientY
-		});
-		TweenLite.to($follow, 0.7, {
-			x: e.clientX,
-			y: e.clientY
-		});
-	}
-	function hoverFunc(e) {
-		TweenLite.to($circle, 0.3, {
-			opacity: 1,
-			scale: 0
-		});
-		TweenLite.to($follow, 0.3, {
-			scale: 2
-		});
-	}
-	function unhoverFunc(e) {
-		TweenLite.to($circle, 0.3, {
-			opacity: 1,
-			scale: 1
-		});
-		TweenLite.to($follow, 0.3, {
-			scale: 1
-		});
-	}
-	$(window).on('mousemove', moveCircle);
-	$("a, .clink").hover(hoverFunc, unhoverFunc);
+	// var $circle = $('.circle'),
+	// 	$follow = $('.circle-follow');
+	// function moveCircle(e) {
+	// 	TweenLite.to($circle, 0.3, {
+	// 		x: e.clientX,
+	// 		y: e.clientY
+	// 	});
+	// 	TweenLite.to($follow, 0.7, {
+	// 		x: e.clientX,
+	// 		y: e.clientY
+	// 	});
+	// }
+	// function hoverFunc(e) {
+	// 	TweenLite.to($circle, 0.3, {
+	// 		opacity: 1,
+	// 		scale: 0
+	// 	});
+	// 	TweenLite.to($follow, 0.3, {
+	// 		scale: 2
+	// 	});
+	// }
+	// function unhoverFunc(e) {
+	// 	TweenLite.to($circle, 0.3, {
+	// 		opacity: 1,
+	// 		scale: 1
+	// 	});
+	// 	TweenLite.to($follow, 0.3, {
+	// 		scale: 1
+	// 	});
+	// }
+	// $(window).on('mousemove', moveCircle);
+	// $("a, .clink").hover(hoverFunc, unhoverFunc);
 
 
 	// Gsap Text Animations
